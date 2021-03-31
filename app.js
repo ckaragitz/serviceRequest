e3dbc249e233b7d3c4e23bd3a003c9126fdf6fd1
Lohit <lohitkrishna39@gmail.com>
2021-03-31 12:54:18 +0530

    correct package.json file

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/home.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
