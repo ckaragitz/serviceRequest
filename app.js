721c9e0fffb0febd437d149e3dac1222211371be
Lohit <lohitkrishna39@gmail.com>
2021-03-31 13:22:05 +0530

    updated listener

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ app.use(express.static(__dirname + '/'));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
+});
