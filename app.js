fc02efba897cffe667a1b1b1c644840d1636f061
Lohit <lohitkrishna39@gmail.com>
2021-03-31 12:58:34 +0530

    added path

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
