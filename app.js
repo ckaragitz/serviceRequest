78d0572023b5944c8c5af3cbbdbf68f6a9ff253f
Lohit <lohitkrishna39@gmail.com>
2021-04-01 17:13:09 +0530

    Removed the bot

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+/*Allow CORS*/
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+    res.setHeader('Access-Control-Max-Age', '1000');
+
+    next();
+});
+
+app.get('/runflow', function (req, res) {
+    res.sendFile(path.join(__dirname + '/runflow.html'));
+});
+
+
 app.listen(port, function () {
     console.log('Example app listening on port 3000!');
 });
